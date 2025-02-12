8ae615b657e9d542718a63e1b996d6f1053bbba0
sarahcoral <43325518+sarahcoral@users.noreply.github.com>
2025-02-12 14:57:25 -0500

    working month predictive_analysis

---
+++ load_database.py
@@ -0,0 +1,25 @@
+def load_db():
+    import os
+    import requests
+    
+    #file_path = os.path.join(os.getcwd(),r"linkedindatabase.db")
+    file_path = r"linkedindatabase.db"
+    
+    if not os.path.exists(file_path):
+        print('Database not ready. Loading...')
+        try:
+            # URL of your Google Drive database file (direct download link)
+            url = "https://drive.google.com/uc?id=1e_PSowuukgifVA7rzqkvhDuCud-YDxtN"
+            # Send a GET request to download the file
+            response = requests.get(url)
+            # Save the content to a local file (in the Binder container or local disk)
+            with open('linkedindatabase.db', 'wb') as f:
+                f.write(response.content)
+            print("Database available!")
+            return 1
+        except Exception as e:
+            print("Failed to fetch database: {e}")
+            return 0
+    else:
+        print('Database already loaded!')
+        return 2
